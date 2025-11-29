c8932035f6e980e723caff8f41a54ffbd53b4a8a
Keiner_Frontado <keinerfrontado@gmail.com>
2025-11-29 12:24:59 -0400

    feat: add Chip Row component

---
+++ lib/components/chip_row.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/constants/styles.dart';
+
+class ChipRow extends StatelessWidget {
+
+  final List<String> buttonTexts;
+  const ChipRow({super.key, required this.buttonTexts});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: buttonTexts.map((text) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: Chip(
+              // 1. Avatar: El icono a la izquierda (opcional)
+              avatar: null,
+              
+              // 2. Label: El texto principal
+              label: Text(
+                text,
+                style: Styles.buttonText
+              ),
+
+              // 3. Estilos visuales
+              backgroundColor: Styles.buttonSecondaryBg,
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              
+              // Eliminar el borde gris por defecto del Chip si quieres que sea plano
+              side: const BorderSide(color: Colors.transparent, width: 0),
+              
+              // Asegurar la forma totalmente redondeada
+              shape: const StadiumBorder(), 
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
--- lib/screens/singleplayer/singleplayer.dart
+++ lib/screens/singleplayer/singleplayer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/components/app_view.dart';
 import 'package:flutter_application_1/components/board_canva.dart';
+import 'package:flutter_application_1/components/chip_row.dart';
 import 'package:flutter_application_1/core/constants/game_themes.dart';
 import 'package:flutter_application_1/core/constants/styles.dart';
 import 'package:flutter_application_1/core/models/board.dart';
@@ -16,13 +17,26 @@ class _SingleplayerScreen extends State<SingleplayerScreen> {
   
   late Board? board;
 
+  Widget _setTitle(){
+    if (board == null) return Text("UN JUGADOR", style: Styles.titleText);
+    return Text(
+      board!.getThemeName(),
+      style: Styles.titleText
+    );
+  }
+
   Widget _setSubtitle() {
     
     if (board != null) {
-      return Text(
-        board!.getThemeName(),
-        style: Styles.text
-        );
+      return Column(
+        children: [
+        ChipRow(
+          buttonTexts: board!.theme.words
+              .where((w) => !board!.foundWords.contains(w))
+              .toList(),
+        ),
+        ]
+      );
     } else {
       return Text(
         "Presiona el botón para iniciar el juego.",
@@ -68,7 +82,7 @@ class _SingleplayerScreen extends State<SingleplayerScreen> {
   @override
   Widget build(BuildContext context) {
     return AppView(
-      title: Text("UN JUGADOR", style: Styles.titleText),
+      title: _setTitle(),
       subtitle: _setSubtitle(),
       footer: _setFooter(),
       child: _setChild(),
